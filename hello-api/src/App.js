d94b00530de7239605369ff72510beac29259c7a
asrisitorus04 <asrisitorus04@gmail.com>
2022-09-25 23:39:53 +0700

    Api Edit

--- hello-api/src/App.js
+++ hello-api/src/App.js
@@ -6,51 +6,88 @@ class App extends Component {
     super(props);
     this.state = {
       dataApi: [],
-      dataPost:{
+      edit: false,
+      dataPost: {
         id: 0,
         title: '',
         body: ''
       }
     };
-    this.handleRemove=this.handleRemove.bind(this);
-    this.inputChange=this.inputChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.inputChange = this.inputChange.bind(this);
     // this.onSubmitForm=this.onSubmitForm.bind(this);
   }
 
-  reloadData(){
+  reloadData() {
     axios.get('http://localhost:3004/posts').then(
       res => {
         this.setState({
-          dataApi:res.data
+          dataApi: res.data,
+          edit: false
         })
       }
     );
   }
 
-  handleRemove(e){
+  handleRemove(e) {
     console.log(e.target.value)
-    fetch(`http://localhost:3004/posts/${e.target.value}`,{method:"DELETE"}).then(res=>this.reloadData())
+    fetch(`http://localhost:3004/posts/${e.target.value}`, { method: "DELETE" }).then(res => this.reloadData())
   }
 
-  inputChange(e){
-    let newdataPost = {...this.state.dataPost};
+  inputChange(e) {
+    let newdataPost = { ...this.state.dataPost };
+    if (this.state.edit === false) {
+      newdataPost["id"] = new Date().getTime();
+    }
     newdataPost['id'] = new Date().getTime();
     newdataPost[e.target.name] = e.target.value;
 
     this.setState({
-      dataPost : newdataPost
-    }, ()=> console.log(this.state.dataPost))
+      dataPost: newdataPost
+    }, () => console.log(this.state.dataPost))
+  }
+  clearData = () => {
+    let newdataPost = { ...this.state.dataPost };
+
+    newdataPost['id'] = "";
+    newdataPost['body'] = "";
+    newdataPost['title'] = "";
+
+    this.setState({
+      dataPost: newdataPost
+    });
   }
 
-  onSubmitForm= () => {
-    axios.post(`http://localhost:3004/posts/`,this.state.dataPost)
-    .then(() => {
-      this.reloadData();
-    });
+  onSubmitForm = () => {
+    if (this.state.edit === false) {
+      axios.post(`http://localhost:3004/posts/`, this.state.dataPost)
+        .then(() => {
+          this.reloadData();
+          this.clearData();
+
+        });
+    } else {
+      axios.put(`http://localhost:3004/posts/${this.state.dataPost.id}`, this.state.dataPost).then(() => {
+        this.reloadData();
+        this.clearData();
+
+      })
+    }
   };
 
+  getDataId = e => {
+    axios
+      .get(`http://localhost:3004/posts/${e.target.value}`)
+      .then(res => {
+        this.setState({
+          dataPost: res.data
+          // edit: true
+        })
+      });
+  }
 
-  
+
+
   componentDidMount() {
     // fetch("https://jsonplaceholder.typicode.com/posts")
     //   .then(response => response.json())
@@ -69,15 +106,37 @@ class App extends Component {
     return (
       <div>
         <p>Hello API</p>
-        <input type='text' name="body" placeholder="Masukkan Body" onChange={this.inputChange}/>
-        <input type="text" name="title" placeholder="Masukkan Title" onChange={this.inputChange} />
-        <button type="Submit" onClick={this.onSubmitForm}> Add Data</button>
+        <input
+          type='text'
+          name="body"
+          value={this.state.dataPost.body}
+          placeholder="Masukkan Body"
+          onChange={this.inputChange}
+        />
+        <input
+          type="text"
+          name="title"
+          value={this.state.dataPost.title}
+          placeholder="Masukkan Title"
+          onChange={this.inputChange}
+        />
+        <button
+          type="Submit"
+          onClick={this.onSubmitForm}>
+          Save Data
+        </button>
         {this.state.dataApi.map((dat, index) => {
-          return (<div key={index}>
-            <p>{dat.body}</p>
-            <button value={dat.id} onClick={this.handleRemove}>Delete</button>
-          </div>)
-        })}
+          return (
+            <div key={index}>
+              <p>{dat.body}</p>
+              <button value={dat.id} onClick={this.handleRemove}>
+                Delete
+              </button>
+              <button value={dat.id} onClick={this.getDataId}>
+                Edit data
+              </button>
+            </div>)
+          })}
       </div>
     )
   };
